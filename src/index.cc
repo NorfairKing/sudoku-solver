e79963fc4d02a2a5d9ea436a4c251ad9ce9a4abf
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-28 00:20:52 +0200

    more index tests

--- src/index.cc
+++ src/index.cc
@@ -1,6 +1,18 @@
 #include "constants.h"
 #include "index.h"
 
+int index(int row, int col) {
+  return col * SIZE + row;
+}
+
+int indexByBox(int box, int i) {
+  int blockRow = box % BLOCK_SIZE;
+  int blockCol = box / BLOCK_SIZE;
+  int rowInBlock = i % BLOCK_SIZE;
+  int colInBlock = i / BLOCK_SIZE;
+  return index(blockRow * BLOCK_SIZE + rowInBlock, blockCol * BLOCK_SIZE + colInBlock);
+}
+
 int boxIndex(int r, int c) {
   int boxRow = r / BLOCK_SIZE;
   int boxCol = c / BLOCK_SIZE;
--- src/index.h
+++ src/index.h
@@ -1,6 +1,9 @@
 #ifndef index_h__
 #define index_h__
 
+int index(int row, int col);
+int indexByBox(int box, int i);
+int boxByBox(int box, int i);
 int boxIndex(int r, int c);
 int indexInBox(int r, int c);
 
